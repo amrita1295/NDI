d60aeaf0ee3b0eaa0d4377fa58bb9f4abcdeb899
amrita jassal <amrita.singh@trainingmug.com>
2023-05-06 14:17:37 +0530

    logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,16 @@
 import React from "react";
 import func from "../../images/func1.png"
 import "../Navbar/Navbar.css"
-const Navbar=()=>{
-return (
-    
-<nav class="bg-[#FBF8F0] border-gray-200 dark:bg-gray-900 h-14 -mt-5">
-  <div class="max-w-screen-xl  p-10 ">
-    <a href="https://ndi.myfunc.in/" class="flex items-center">
-        <img src={func} className="h-15 w-28 mr-3 pic" alt="MyFunc Logo "  />
-    </a>
-    
-  </div>
-</nav>
+const Navbar = () => {
+    return (
+        <div className="navbar  bg-[#FBF8F0]">
+            <a className="btn btn-ghost normal-case text-xl">
+                <img src={func} alt="" className="h-15 w-28 "/>
+            </a>
+        </div>
 
-)
+
+
+    )
 }
 export default Navbar
