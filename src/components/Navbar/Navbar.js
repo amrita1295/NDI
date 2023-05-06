e5497839f38dcc0507658cad1e4be5f6cbd03b5c
amrita jassal <amrita.singh@trainingmug.com>
2023-05-06 13:25:51 +0530

    logo

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Navbar=()=>{
+return (
+    
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center">
+        <img src="" class="h-8 mr-3" alt="Flowbite Logo" />
+    </a>
+    
+  </div>
+</nav>
+
+)
+}
+export default Navbar
